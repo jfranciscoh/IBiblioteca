20e0f8fd51117cefd8050e9626e73f31a0d3a21b
JuanFranciscoHerrera <JuanFranciscoHerrera@DESKTOP-G6N0T9M>
2023-03-30 00:08:16 -0300

    index add

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/addbook')
+@app.route('/')
 def add_book():
-    return render_template('addbook.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

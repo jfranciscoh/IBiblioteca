ac255bc355844e5df4d4df91290b90e309eafb24
JuanFranciscoHerrera <JuanFranciscoHerrera@DESKTOP-G6N0T9M>
2023-03-30 11:10:15 -0300

    menu

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def add_book():
+def index():
     return render_template('index.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
